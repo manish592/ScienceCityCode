74dbc1b2f8eeb97aa793dbac29892b4492df6af9
manish sharma <sharma.manishmca@gmail.com>
2020-12-27 19:46:00 +0530

    done

---
+++ Science.City.EF.dbmodel/Migrations/20190219133219_asasasasa.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Science.City.EF.dbmodel.Migrations
+{
+    public partial class asasasasa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NewsPaperCompany",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PressIDCard",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PressReporterName",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NewsPaperCompany",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PressIDCard",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PressReporterName",
+                table: "Users");
+        }
+    }
+}
